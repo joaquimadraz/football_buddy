7c5642acbd22049425ba3dd5b10dfd3db9416300
joaquimadraz <joaquim.adraz@gmail.com>
2017-09-03 18:14:05 +0100

    Add redux composeEnhancers for dev tools

--- apps/football_buddy_web/frontend/src/data/store.js
+++ apps/football_buddy_web/frontend/src/data/store.js
@@ -1,17 +1,24 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import { routerMiddleware } from 'react-router-redux'
 import { browserHistory } from 'react-router'
 import rootReducer from './rootReducer'
 
-const initialState = {}
+import initialState from './initialState'
 
-export default function initStore() {
-  return createStore(
-    rootReducer,
-    initialState,
+// For Redux Devtools:
+// https://github.com/zalmoxisus/redux-devtools-extension#12-advanced-store-setup
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const initStore = () => createStore(
+  rootReducer,
+  initialState,
+  composeEnhancers(
     applyMiddleware(
+      thunkMiddleware,
       routerMiddleware(browserHistory),
     ),
   )
-}
+)
+
+export default initStore
